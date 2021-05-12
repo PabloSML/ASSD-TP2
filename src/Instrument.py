31aeb4d4f520832f5532218f7164fa384429ef6d
Ricardo0623 <xlin@itba.edu.ar>
2021-05-11 21:11:45 -0300

    Karplus-Strong for drums

--- src/Instrument.py
+++ src/Instrument.py
@@ -1,4 +1,5 @@
 from src.karplusStrong import karplus_strong
+from src.karplusStrong import karplus_strong_drum
 
 class Instrument:
     def __init__(self):
@@ -27,4 +28,24 @@ class GuitarString(Instrument):
         self.amplitude = amplitude
         self.init_wavetable()
         self.sample = karplus_strong(self.wavetable, self.duration * self.fs, self.stretch_factor)
+        return self.sample
+
+
+class Drum(Instrument):
+    def __init__(self):
+        super(Drum, self).__init__()
+        pass
+
+    def init_wavetable(self):
+        self.L = self.fs // int(self.noteFrequency)
+        self.wavetable = (self.amplitude * 2 * np.random.randint(0, 2, self.L) - 1).astype(np.float)
+
+    def play_note(self, noteFrequency, duration, fs, stretch_factor=1, amplitude=1):
+        self.noteFrequency = noteFrequency
+        self.duration = duration
+        self.fs = fs
+        self.stretch_factor = stretch_factor
+        self.amplitude = amplitude
+        self.init_wavetable()
+        self.sample = karplus_strong_drum(self.wavetable, self.duration * self.fs)
         return self.sample
