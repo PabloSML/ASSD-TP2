c076cce09bd0420be4830de1d3b791c2f517aed3
Ricardo0623 <xlin@itba.edu.ar>
2021-05-11 21:35:38 -0300

    Nueva carpeta KS

--- src/Instrument.py
+++ src/Instrument.py
@@ -1,5 +1,3 @@
-from src.karplusStrong import karplus_strong
-from src.karplusStrong import karplus_strong_drum
 
 class Instrument:
     def __init__(self):
@@ -8,44 +6,3 @@ class Instrument:
     def play_note(self, noteFrequency, duration, fs, **kwargs):
         raise Exception('play_note function must be overwritten for Instrument child')
 
-
-class GuitarString(Instrument):
-    def __init__(self):
-        super(GuitarString, self).__init__()
-        pass
-
-    def init_wavetable(self):
-        """Generates a new wavetable for the string."""
-        self.L = self.fs // int(self.noteFrequency)
-        self.wavetable = (self.amplitude * 2 * np.random.randint(0, 2, self.L) - 1).astype(np.float)
-
-    def play_note(self, noteFrequency, duration, fs, stretch_factor=1, amplitude=1):
-        """Returns next sample from string."""
-        self.noteFrequency = noteFrequency
-        self.duration = duration
-        self.fs = fs
-        self.stretch_factor = stretch_factor
-        self.amplitude = amplitude
-        self.init_wavetable()
-        self.sample = karplus_strong(self.wavetable, self.duration * self.fs, self.stretch_factor)
-        return self.sample
-
-
-class Drum(Instrument):
-    def __init__(self):
-        super(Drum, self).__init__()
-        pass
-
-    def init_wavetable(self):
-        self.L = self.fs // int(self.noteFrequency)
-        self.wavetable = (self.amplitude * 2 * np.random.randint(0, 2, self.L) - 1).astype(np.float)
-
-    def play_note(self, noteFrequency, duration, fs, stretch_factor=1, amplitude=1):
-        self.noteFrequency = noteFrequency
-        self.duration = duration
-        self.fs = fs
-        self.stretch_factor = stretch_factor
-        self.amplitude = amplitude
-        self.init_wavetable()
-        self.sample = karplus_strong_drum(self.wavetable, self.duration * self.fs)
-        return self.sample
---
+++ src/KarplusStrong/ksInstruments.py
@@ -0,0 +1,44 @@
+from src.KarplusStrong.karplusStrong import karplus_strong
+from src.KarplusStrong.karplusStrong import karplus_strong_drum
+from src.Instrument import Instrument
+import numpy as np
+class GuitarString(Instrument):
+    def __init__(self):
+        super(GuitarString, self).__init__()
+        pass
+
+    def init_wavetable(self):
+        """Generates a new wavetable for the string."""
+        self.L = self.fs // int(self.noteFrequency)
+        self.wavetable = (self.amplitude * 2 * np.random.randint(0, 2, self.L) - 1).astype(np.float)
+
+    def play_note(self, noteFrequency, duration, fs, stretch_factor=1, amplitude=1):
+        """Returns next sample from string."""
+        self.noteFrequency = noteFrequency
+        self.duration = duration
+        self.fs = fs
+        self.stretch_factor = stretch_factor
+        self.amplitude = amplitude
+        self.init_wavetable()
+        self.sample = karplus_strong(self.wavetable, self.duration * self.fs, self.stretch_factor)
+        return self.sample
+
+
+class Drum(Instrument):
+    def __init__(self):
+        super(Drum, self).__init__()
+        pass
+
+    def init_wavetable(self):
+        self.L = self.fs // int(self.noteFrequency)
+        self.wavetable = (self.amplitude * 2 * np.random.randint(0, 2, self.L) - 1).astype(np.float)
+
+    def play_note(self, noteFrequency, duration, fs, stretch_factor=1, amplitude=1):
+        self.noteFrequency = noteFrequency
+        self.duration = duration
+        self.fs = fs
+        self.stretch_factor = stretch_factor
+        self.amplitude = amplitude
+        self.init_wavetable()
+        self.sample = karplus_strong_drum(self.wavetable, self.duration * self.fs)
+        return self.sample
