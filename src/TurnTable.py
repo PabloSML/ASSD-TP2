377224d8cd360897b7e20aecbef43cb09c733dbf
PabloSmolkin <psmolkin@itba.edu.ar>
2021-05-13 02:43:35 -0300

    No anda pyAudio

--- src/TurnTable.py
+++ src/TurnTable.py
@@ -5,6 +5,8 @@ from src.Track import Track
 from src.SamAsh import SamAsh
 import soundfile as sf
 import pyaudio
+import simpleaudio as sa
+
 
 class TurnTable:
 
@@ -32,7 +34,7 @@ class TurnTable:
         self.song = np.zeros(self.songLength)
 
         tracksCreated = 0
-        for index, track in enumerate(self.midi_file.tracks[1:]):
+        for index, track in enumerate(self.midi_file.tracks[1:2]):
             lastEv = track[-2]  # Sin contar End-Of-Track
             if lastEv.type.find('note') != -1 or lastEv.type.find('control') != -1:  # Ver que la track sea de notas
                 self.trackList.append(Track(midiLength=self.songLength, midiTrack=track, trackNumber=index+1,
@@ -50,33 +52,38 @@ class TurnTable:
                 track.synthesize_track()
         print('Synthesized all tracks')
 
-    def chunks(self, lst, n):
-        """Yield successive n-sized chunks from lst."""
-        for i in range(0, len(lst), n):
-            yield lst[i:i + n]
+    # def chunks(self, lst, n):
+    #     """Yield successive n-sized chunks from lst."""
+    #     for i in range(0, len(lst), n):
+    #         yield lst[i:i + n]
 
     def play_synthesis(self):
         chunkSize = 1024
         # instancio PyAudio
         p = pyaudio.PyAudio()
         # abrimos player
-        player = p.open(output=True)
+        player = p.open(rate=self.fs, channels=1, output=True, format=pyaudio.paFloat32)
+        player.start_stream()
 
         # arreglo para tener copia de los audios e ir "extrayendo chunks de c/u"
         self.normalize_size()
 
         chunkIndex = 0
 
-        playbackData = np.zeros(chunkSize)
-        while chunkIndex*chunkSize < self.song.size:
-            for track in self.trackList:
-                chunkAudio = track.audioTrack[chunkIndex*chunkSize: (chunkIndex+1)*chunkSize]
-                # applyFX(chunkAudio, track.FXConnections)
-                playbackData += chunkAudio
 
-            player.write(playbackData)
-            chunkIndex += 1
+        # while chunkIndex*chunkSize < self.song.size:
+        #
+        #     playbackData = np.zeros(chunkSize).astype(np.float32)
+        #
+        #     for track in self.trackList:
+        #         chunkAudio = track.audioTrack[chunkIndex*chunkSize: (chunkIndex+1)*chunkSize].astype(np.float32)
+        #         # applyFX(chunkAudio, track.FXConnections)
+        #         playbackData += chunkAudio
+        #
+        #     player.write(playbackData)
+        #     chunkIndex += 1
 
+        player.write(self.trackList[0].audioTrack.tobytes())
 
         player.stop_stream()
         player.close()
@@ -88,10 +95,10 @@ class TurnTable:
             if maxLenTrack < track.audioTrack.size:
                 maxLenTrack += track.audioTrack.size - maxLenTrack
             elif maxLenTrack > track.audioTrack.size:
-                track.audioTrack = np.append(track.audioTrack, maxLenTrack - track.audioTrack.size)
+                track.audioTrack = np.append(track.audioTrack, np.zeros(maxLenTrack - track.audioTrack.size))
 
         if maxLenTrack > self.songLength:
-            self.song = np.append(self.song, maxLenTrack - self.songLength)
+            self.song = np.append(self.song, np.zeros(maxLenTrack - self.songLength))
 
 
     def save_synthesis(self):
@@ -112,4 +119,4 @@ class TurnTable:
 beogram4000C = TurnTable()
 beogram4000C.load('D:/PycharmProjects/ASSD-TP2/tests/rodriG.mid')
 beogram4000C.synthesize()
-beogram4000C.save_synthesis()
+beogram4000C.play_synthesis()
