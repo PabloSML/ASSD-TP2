8f2dc5419762e0fca89caf771f4486c7a88b3336
PDutriez <42210994+PDutriez@users.noreply.github.com>
2021-05-14 02:37:21 -0300

    first GUI

---
+++ app_main.py
@@ -0,0 +1,20 @@
+import sys
+from src.ui.mainwindow import Ui_Form
+from PyQt5 import QtCore, QtGui, QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas, \
+    NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+# from src import UniFilter as UF
+# from src.lib import handy as hand
+# from src.lib.PlotControl import plotControl
+import numpy as np
+import scipy.signal as ss
+
+
+class AppClass(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):  # instanciamos la clase
+        super(AppClass, self).__init__(parent)
+
+        self.ui =Ui_Form()
+        self.ui.setupUi(self)
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 # Project modules
-from src.app import main
+import src.ui.mainwindow as ou
+import app_main as AC
+import sys
 
-if __name__ == "__main__":
-    main()
+
+
+if __name__ == '__main__':
+    #print(AC.QtWidgets.QStyleFactory.keys())
+    MyFilterToolApp = AC.QtWidgets.QApplication(sys.argv)
+    MyFilterToolApp.setStyle(AC.QtWidgets.QStyleFactory.create("Fusion"))
+    MyFilterTool = AC.AppClass()
+    MyFilterTool.show()
+    sys.exit(MyFilterToolApp.exec_())
+
