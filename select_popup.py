58ccb0d453b381c92c84796d0804b12f70c290b7
PabloSmolkin <psmolkin@itba.edu.ar>
2021-05-14 12:36:24 -0300

    Select popup

---
+++ select_popup.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import QDialog
+from src.ui.popup import Ui_Dialog
+
+
+class instrument_selector(QDialog, Ui_Dialog):
+
+    def __init__(self):
+        super(instrument_selector, self).__init__()
+        self.setupUi()
+
